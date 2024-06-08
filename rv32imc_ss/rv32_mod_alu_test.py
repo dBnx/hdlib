f30698c69040b06df7e0b19a9b6b4c8210f6edcc
David Bänsch <d.b@nsch.at>
2024-06-08 15:02:24 +0200

    test(hart): Add templates for unit tests

---
+++ rv32imc_ss/rv32_mod_alu_test.py
@@ -0,0 +1,51 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, First
+# from cocotb.handle import Freeze, Release
+from dataclasses import dataclass
+
+
+@cocotb.test()
+async def test_foo(dut) -> None:
+    clk_time_ns = int(1e9 / dut.CLK_FREQ_HZ.value)
+    cocotb.start_soon(Clock(dut.clk, clk_time_ns, units="ns").start())
+    await Timer(1, "ps")
+
+    # await reset_dut(dut)
+
+    # timing_check_h = cocotb.start_soon(vga_timing_checker_hsync(dut))
+    # timing_check_v = cocotb.start_soon(vga_timing_checker_vsync(dut))
+
+    # await First(test_time, timing_check_h, timing_check_v)
+
+
+def test_runner():
+    import os
+    from pathlib import Path
+    from cocotb.runner import get_runner
+
+    hdl_toplevel = "rv32_mod_instruction_decoder"
+    sim = os.getenv("SIM", "verilator")
+    project_path = Path(__file__).resolve().parent
+
+    verilog_sources = [
+        project_path / f"{hdl_toplevel}.sv",
+    ]
+
+    build_args = ["--trace", "--trace-structs"] if sim == "verilator" else []
+    runner = get_runner(sim)
+    runner.build(
+        verilog_sources=verilog_sources,
+        vhdl_sources=[],
+        hdl_toplevel=hdl_toplevel,
+        always=True,
+        build_args=build_args,
+        build_dir=f"build/{hdl_toplevel}",
+    )
+
+    runner.test(hdl_toplevel=hdl_toplevel, test_module=f"{hdl_toplevel}_test,",
+                waves=True, extra_env={"WAVES": "1"})
+
+
+if __name__ == "__main__":
+    test_runner()
