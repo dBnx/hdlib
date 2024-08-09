0b740787aa42dfce66f4281bd9eee72f65831218
David Bänsch <d.b@nsch.at>
2024-08-09 20:41:35 +0200

    test(hart-branch): Add test template

--- rv32imc_ss/rv32_mod_branch_test.py
+++ rv32imc_ss/rv32_mod_branch_test.py
@@ -3,28 +3,58 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, First
 # from cocotb.handle import Freeze, Release
 from dataclasses import dataclass
+import random
+# from random import random
 
+# TODO: Implement
+
+# module rv32_mod_branch (
+#     input  [31:0] rf_read0,
+#     input  [31:0] rf_read1,
+#     input  [ 2:0] cond,
+#     input         is_cond,
+#     input         is_jmp,
+#     output        branch_taken
+# );
+
+### Test functions ###
 
 @cocotb.test()
-async def test_foo(dut) -> None:
-    clk_time_ns = int(1e9 / dut.CLK_FREQ_HZ.value)
-    cocotb.start_soon(Clock(dut.clk, clk_time_ns, units="ns").start())
-    await Timer(1, "ps")
+async def test_jump_always_taken(dut) -> None:
+    for _ in range(16):
+        take_branch = random.getrandbits(1)
 
-    # await reset_dut(dut)
+        dut.is_jmp.value = take_branch
+        dut.is_cond.value = random.getrandbits(1) if take_branch == 1 else 0
+        dut.cond.value = random.getrandbits(2)
+        dut.rf_read0.value = random.getrandbits(32)
+        dut.rf_read1.value = random.getrandbits(32)
 
-    # timing_check_h = cocotb.start_soon(vga_timing_checker_hsync(dut))
-    # timing_check_v = cocotb.start_soon(vga_timing_checker_vsync(dut))
+        await Timer(1, "ps")
 
-    # await First(test_time, timing_check_h, timing_check_v)
+        if take_branch != dut.branch_taken.value:
+            base = "Branch not taken, even tough it should" if take_branch == 1 else \
+                            "Branch taken, even tough it shouldn't "
+            rf0 = f"{int(dut.rf_read0.value):4X}"
+            rf1 = f"{int(dut.rf_read1.value):4X}"
+            details = f"is_jmp={dut.is_jmp.value} is_cond={dut.is_cond.value} cond={dut.cond.value} rf0={rf0} rf1={rf1}"
+            
+            assert dut.branch_taken.value == take_branch, f"{base}: {details}"
 
+        await Timer(1, "ps")
+
+### Helper functions ###
+
+### Runner ###
 
 def test_runner():
     import os
     from pathlib import Path
     from cocotb.runner import get_runner
 
-    hdl_toplevel = "rv32_mod_instruction_decoder"
+    random.seed()
+
+    hdl_toplevel = "rv32_mod_branch"
     sim = os.getenv("SIM", "verilator")
     project_path = Path(__file__).resolve().parent
 
