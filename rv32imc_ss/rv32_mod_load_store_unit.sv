fad13e4601b00d2c45046e7392e59b53cfa3b1cc
David Bänsch <d.b@nsch.at>
2024-08-25 17:43:10 +0200

    test(hart-lsu): Add test template

--- rv32imc_ss/rv32_mod_load_store_unit.sv
+++ rv32imc_ss/rv32_mod_load_store_unit.sv
@@ -1,108 +1,196 @@
-module rv32_mod_load_store_unit (
-    input        clk,
-    input        reset,
+/*verilator tracing_on*/
+`timescale 1ps / 1ps
 
-    // HART Interface
-    input        req,
-    input [ 3:0] req_type, // [S,U]; Reserved; Size
-    input        wr,
-    input [31:0] address,
-    input [31:0] data_i,
+// Every active req issues a new request.
+// Requests can be issued unless stall is raised.
+// New issues are accepted, while old ones are served.
+// Min latency: 1 cycle, as everything is registerd without skid buffers
+// Some computation (sign) is done on dext_di, so it may not be stable too
+// late in a cycle
+module rv32_mod_load_store_unit (
+    input clk,
+    input reset,
+
+    // HART interface
+    input               req,
+    input        [ 3:0] req_type,  // [S,U]; Reserved; Size
+    input               wr,
+    input        [31:0] address,
+    input        [31:0] data_i,
     output logic [31:0] data_o,
     output logic        valid,
     output logic        error,
     output logic        stall,
-    
-    // External interf
-    output        dext_req,
-    output        dext_wr,
-    input         dext_ack,
-    input         dext_err,
-    output [ 3:0] dext_be,
-    output [31:0] dext_addr,
-    output [31:0] dext_do,
-    input  [31:0] dext_di
+
+    // External interface
+    output logic        dext_req,
+    output logic        dext_wr,
+    input               dext_ack,
+    input               dext_err,
+    output logic [ 3:0] dext_be,
+    output logic [31:0] dext_addr,
+    output logic [31:0] dext_do,
+    input        [31:0] dext_di
 );
-    logic [ 3:0] dext_be_comb;
-    logic [31:0] dext_di_comb;
+  logic [3:0] dext_be_comb;
+  logic [31:0] dext_di_comb;
 
 
-    logic req_signed;
-    logic [1:0] req_size;
-    assign req_signed = req_type[3];
-    assign req_size = req_type[1:0];
+  logic req_signed;
+  logic [1:0] req_size;
 
-    // Stalling halts the PC, so only emit if there is an actual request
+  logic req_active;
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      req_active <= 0;
+    end else begin
+      // If there is an active rq, that is terminated and no new rq
+      if (req_active && (dext_ack || dext_err) && !req) begin
+        req_active <= 0;
+      end else if (req) begin
+        req_active <= 1;
+      end
+    end
+  end
 
-    logic [31:8] sign;
-    logic sign_16;
-    logic sign_8;
-    logic sign_bit;
-    assign sign_16 = address[1] ? dext_di[31] : dext_di[15];
-    assign sign = {24{sign_bit}};
 
-    // FIXME: Add handling of aligned store / loads
-    always_comb begin
-        case(req_size)
-        2'b00: dext_be_comb = 1'b1 << address[1:0];
-        2'b01: dext_be_comb = address[1] ? 4'b1100 : 4'b0011;
-        2'b10: dext_be_comb = 4'b1111;
-        default: dext_be_comb = 0;
+  // Stalling halts the PC, so only emit if there is an actual request
+
+  logic [31:8] sign;
+  logic sign_16;
+  logic sign_8;
+  logic sign_bit;
+  assign sign_16 = address[1] ? dext_di[31] : dext_di[15];
+  assign sign = {24{sign_bit}};
+
+  // FIXME: Add handling of unaligned store / loads
+  always_comb begin
+    // dext_be_comb is registered with the request, so calc from input
+    case (req_type[1:0])  // == req_size
+      //  8-bit
+      2'b00:   dext_be_comb = 1'b1 << address[1:0];
+      // 16-bit
+      2'b01:   dext_be_comb = address[1] ? 4'b1100 : 4'b0011;
+      // 32-bit
+      2'b10:   dext_be_comb = 4'b1111;
+      default: dext_be_comb = 0;
+    endcase
+
+    // TODO: Fix timing and use external address?
+    case (dext_addr[1:0])
+      2'b00:   sign_8 = dext_di[7];
+      2'b01:   sign_8 = dext_di[15];
+      2'b10:   sign_8 = dext_di[23];
+      2'b11:   sign_8 = dext_di[31];
+      default: sign_bit = 0;
+    endcase
+
+    case ({
+      req_signed, req_size
+    })
+      3'b101:  sign_bit = sign_8;
+      3'b110:  sign_bit = sign_16;
+      default: sign_bit = 0;
+    endcase
+
+    case (req_size)
+      2'b00: begin  // 8b
+        case (dext_addr[1:0])
+          2'b00:   dext_di_comb = {sign[31:8], dext_di[7:0]};
+          2'b01:   dext_di_comb = {sign[31:8], dext_di[15:8]};
+          2'b10:   dext_di_comb = {sign[31:8], dext_di[23:16]};
+          2'b11:   dext_di_comb = {sign[31:8], dext_di[31:24]};
+          default: dext_di_comb = 0;
         endcase
+      end
+      2'b01: begin  // 16b
+        dext_di_comb = address[1] ? {sign[31:16], dext_di[31:16]} : {sign[31:16], dext_di[31:16]};
+      end
+      2'b10: begin  // 32b
+        dext_di_comb = dext_di;
+      end
+      default: begin  // 64b (not implemented)
+        dext_di_comb = 0;
+      end
+    endcase
+  end
 
-        case(address[1:0])
-        2'b00: sign_8 = dext_di[7];
-        2'b01: sign_8 = dext_di[15];
-        2'b10: sign_8 = dext_di[23];
-        2'b11: sign_8 = dext_di[31];
-        default: sign_bit = 0;
-        endcase
+  // HART Output
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      dext_req <= 0;
+      dext_wr <= 0;
+      dext_addr <= 0;
+      dext_be <= 0;
+      dext_do <= 0;
 
-        case({req_signed, req_size})
-        3'b101: sign_bit = sign_8;
-        3'b110: sign_bit = sign_16;
-        default: sign_bit = 0;
-        endcase
+      stall <= 0;
+      req_signed <= 0;
+      req_size <= 0;
+    end else if (!req_active && !req) begin
+      // Reset IF only if no active or incomming rq
+      dext_req <= 0;
+      dext_wr <= 0;
+      dext_addr <= 0;
+      dext_be <= 0;
+      dext_do <= 0;
 
-        case(req_size)
-        2'b01: begin
-            case(address[1:0])
-            2'b00: dext_di_comb = {sign[31:8], dext_di[ 7: 0]};
-            2'b01: dext_di_comb = {sign[31:8], dext_di[15: 8]};
-            2'b10: dext_di_comb = {sign[31:8], dext_di[23:16]};
-            2'b11: dext_di_comb = {sign[31:8], dext_di[31:24]};
-            default: dext_di_comb = 0;
-            endcase
-        end
-        2'b10: dext_di_comb = address[1] ? {sign[31:16], dext_di[31:16]} : {sign[31:16], dext_di[31:16]};
-        2'b11: dext_di_comb = dext_di;
-        default: dext_di_comb = 0;
-        endcase
+      stall <= 0;
+      req_signed <= 0;
+      req_size <= 0;
+    end else if (req) begin
+      // Hold?
+      dext_req  <= req;
+      dext_wr   <= wr;
+      dext_addr <= {address[31:2], 2'h0};
+      dext_be   <= dext_be_comb;
+      // dext_do   <= 0;
+
+      if (wr) begin
+        dext_do <= data_i;
+        req_signed <= 0;
+        req_size <= 0;
+      end else begin
+        dext_do <= 0;
+        req_signed <= req_type[3];
+        req_size <= req_type[1:0];
+      end
     end
 
-    assign stall = (!dext_ack && !dext_err) && dext_req;
-    always_ff @( posedge clk or posedge reset ) begin
-        if(reset) begin
-            dext_req <= 0;
-            dext_wr <= 0;
-            dext_addr <= 0;
-            dext_be <= 0;
-        end else begin
-            // Hold?
-            dext_req <= req;
-            dext_wr <= wr;
-            dext_addr <= {address[31:2], 2'h0};
-            dext_be <= dext_be_comb;
-
-            valid = dext_ack;
-            error = dext_err;
-
-            if(wr) begin
-                dext_do <= data_i;
-            end else if(!wr && dext_ack) begin
-                data_o <= dext_di_comb;
-            end
-        end
+    // HART Stall
+    if (reset) begin
+      stall <= 0;
+    end else begin
+      stall <= (!dext_ack && !dext_err) && (req || req_active);
     end
 
+    // HART Input
+    if (reset) begin
+      data_o <= 0;
+      valid  <= 0;
+      error  <= 0;
+    end else if (dext_ack || dext_err) begin
+      valid <= dext_ack;
+      error <= dext_err;
+
+      if (!wr && dext_ack) begin
+        data_o <= dext_di_comb;
+      end else begin
+        data_o <= 0;
+      end
+    end else begin
+      data_o <= 0;
+      valid  <= 0;
+      error  <= 0;
+    end
+  end
+
+`ifdef COCOTB_SIM
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, rv32_mod_load_store_unit);
+    // #1;
+  end
+`endif
 endmodule
+
--- rv32imc_ss/rv32_mod_load_store_unit_test.py
+++ rv32imc_ss/rv32_mod_load_store_unit_test.py
@@ -3,12 +3,15 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, First
 # from cocotb.handle import Freeze, Release
 from dataclasses import dataclass
+import random
 
 REQ_UNSIGNED = 0b0000
 REQ_SIGNED   = 0b1000
 REQ_WIDTH_8  = 0b0000
 REQ_WIDTH_16 = 0b0001
 REQ_WIDTH_32 = 0b0010
+
+# TODO: Currently only 32b works / is tested
 
 async def reset(dut) -> None:
     dut.req.value = 0
@@ -17,6 +20,7 @@ async def reset(dut) -> None:
 
     dut.dext_ack.value = 0
     dut.dext_err.value = 0
+    dut.dext_di.value = 0
 
     dut.reset.value = 1
     await Timer(1, "ps")
@@ -26,8 +30,9 @@ async def reset(dut) -> None:
     dut.reset.value = 0
     await Timer(1, "ps")
 
-# @cocotb.test()
+@cocotb.test()
 async def test_idle(dut) -> None:
+    """ Reset and do not issue a request, then monitor I/O lines """
     cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
 
     await reset(dut)
@@ -59,93 +64,179 @@ async def test_idle(dut) -> None:
         assert 0 == dut.dext_wr.value
         assert 0 == dut.dext_do.value
 
-async def deassert_stall_after(dut, n_cycles: int, then_error: bool = False) -> None:
+async def deassert_stall_after(dut, n_cycles: int, data: int|None = None, then_error: bool = False) -> None:
+    """Mock external interface by:
+    - Assert stall for n_cycles if non-zero
+    - Then asser ack or err, depending on `then_error`
+    - Deassert ack after the next pos edge
+    """
     dut.stall.value = 1
     await Timer(1, "ps")
 
     for _ in range(n_cycles):
         await RisingEdge(dut.clk)
-        await Timer(1, "ps")
+        await Timer(1, "ns")
 
-    if then_error if False:
+    if then_error is False:
         dut.dext_ack.value = 1
+        if data is not None:
+            dut.dext_di.value = data
     else:
-        dut.dext_error.value = 1
+        dut.dext_err.value = 1
 
     await RisingEdge(dut.clk)
-    await Timer(1, "ps")
+    await Timer(1, "ns")
 
-    if then_error if False:
+    if then_error is False:
         dut.dext_ack.value = 0
     else:
-        dut.dext_error.value = 0
+        dut.dext_err.value = 0
 
-@cocotb.test()
-async def test_write32_instant_ack(dut) -> None:
-    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    dut.dext_di.value = 0
 
-    data_word = 0x1234
-    latency = 1
 
-    await reset(dut)
-
-    # Issue write
-    dut.req.value = 1
-    dut.req_type.value = REQ_SIGNED | REQ_WIDTH_32
-    dut.wr.value = 1
-
-    dut.dext_ack.value = 0
-    dut.dext_err.value = 0
-    await RisingEdge(dut.clk)
-
-    # Delay ack by `latency`
-    cocotb.start_soon(deassert_stall_after(dut, latency))
+async def latency_verification(dut, write_read: bool, data: int | None, latency: int):
+    await Timer(2, "ps")
 
     # Check if request is issued
-    await Timer(1, "ps")
-    assert 1 == dut.dext_req.value
-    assert 1 == dut.dext_wr.value
-    assert data == dut.dext_do.value
+    assert 1 == dut.dext_req.value, "No request issued"
+    assert write_read == dut.dext_wr.value, "Wrong request issued"
+    if write_read is True:
+        assert data is not None, "Write needs reference data"
+        assert hex(data) == hex(dut.dext_do.value), "External IF has no data"
 
     # Wait `latency` and don't deassert
     for _ in range(latency):
-        await Timer(1, "ps")
         await RisingEdge(dut.clk)
-        await Timer(1, "ps")
-        assert 1 == dut.dext_req.value
-        assert 1 == dut.dext_wr.value
-        assert data == dut.dext_do.value
+        await FallingEdge(dut.clk) # Wait for the mock to set signals
+        # await Timer(1, "ns") 
+        # await Timer(1, "ps") # Ensure they are propagated
+        assert 1 == dut.dext_req.value, "No request issued"
+        assert write_read == dut.dext_wr.value, "Wrong request issued"
+        if write_read is True:
+            assert hex(data) == hex(dut.dext_do.value), "External IF has no data or doesn't hold"
+
+async def issue_command(dut, write_read: bool, addr: int, data: int | None = None, latency: int = 1, issue_ack: bool = True):
+    assert addr > 0 and data > 0 and latency >= 0, "Invalid values"
+    assert not ( write_read is True and data is None ), "Usage error: Write expects data"
+
+    # Setup operation
+    dut.req.value = 1
+    dut.req_type.value = REQ_SIGNED | REQ_WIDTH_32
+    dut.wr.value = 1 if write_read is True else 0
+    dut.address.value = addr
+    if data is not None:
+        dut.data_i.value = data
+
+    # Issue operation and start bus mock -> Delay ack by `latency`
+    await RisingEdge(dut.clk)
+
+    # Reset IF
+    dut.req.value = 0
+    dut.req_type.value = 0
+    dut.wr.value = 0
+    dut.address.value = 0
+    if data is not None:
+        dut.data_i.value = 0
+
+    cocotb.start_soon(deassert_stall_after(dut, latency, data))
+
+    # Wait for external and verify
+    await latency_verification(dut, write_read, data, latency)
+
+    await return_verification(dut, write_read, expect_ack=issue_ack, data=data)
+
+    # Restore clean state
+    dut.req.value = 0
+    dut.req_type.value = 0
+    dut.wr.value = 0
+
+
+async def return_verification(dut, write_read: bool, expect_ack: bool, data: int | None = None):
+    """Assuming the external bus (mocked) responds in this cycle:
+    - Check for a termination signal on external
+    - Wait one edge to pass through to the HART IF
+    - Verify received signals
+    """
+    assert not ( write_read is True and data is None ), "Usage error: Write expects data"
+
+    await Timer(1, "ns")
 
     # Check if delayed ack is issued
-    assert 1 == dut.dext_ack.value
-    # TODO: Ack takes also 1 cycle to pass through ..
+    if expect_ack is True:
+        assert 1 == dut.dext_ack.value, "Expected external ack"
+        assert 0 == dut.dext_err.value
+    else:
+        assert 0 == dut.dext_ack.value
+        assert 1 == dut.dext_err.value
 
+    # NOTE: Ack takes also 1 cycle to pass through ..
     await RisingEdge(dut.clk)
-    await Timer(1, "ps")
-
-    # TODO: Meaning it's now at the HART Interface
-    # Check if again in idle
-    assert 0 == dut.dext_req.value
-    assert 0 == dut.dext_wr.value
-    assert 0 == dut.valid.value
-    assert 0 == dut.error.value
+    await Timer(1, "ns")
+    
+    assert 1 if expect_ack is True else 0 == dut.valid.value, "Expected ack"
+    assert 1 if expect_ack is False else 0 == dut.error.value, "Expected err"
     assert 0 == dut.stall.value
-    assert 0 == dut.data_o.value
+    if write_read is False and expect_ack:
+        assert data == dut.data_o.value
+
+    # NOTE: Meaning it's now at the HART Interface
+    # TODO: Check if again in idle - not sure why note exists
+    # assert 0 == dut.dext_req.value
+    # assert 0 == dut.dext_wr.value
+    # assert 0 == dut.valid.value
+    # assert 0 == dut.error.value
+    # assert 0 == dut.stall.value
+    # assert 0 == dut.data_o.value
     # TODO: Check stall from 0 to here
 
-    await RisingEdge(dut.clk)
+@cocotb.test()
+async def test_write32_single(dut) -> None:
+    """ Issue writes with zero latency feedback """
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+
+    await reset(dut)
     await Timer(1, "ps")
 
-    # TODO: And now cleared
-    # External Interface
+    latency =random.randint(1,4) 
+    data = random.getrandbits(32)
+    addr = random.getrandbits(32)
 
-    for _ in range(3):
-        await Timer(1, "ps")
-        await RisingEdge(dut.clk)
-        await Timer(1, "ps")
+    await Timer(1, "ns")
+    await issue_command(dut, write_read=True, addr=addr, data=data, latency=latency)
+    await RisingEdge(dut.clk)
+    await FallingEdge(dut.clk)
+    assert 0 == dut.valid
+    assert 0 == dut.stall
 
+@cocotb.test()
+async def test_write32_two_read_back(dut) -> None:
+    """ Issue writes with zero latency feedback """
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+
+    await reset(dut)
+    await Timer(1, "ps")
+
+    data_a = random.getrandbits(32)
+    data_b = random.getrandbits(32)
+    addr_a = random.getrandbits(32)
+    addr_b = random.getrandbits(32)
+    while addr_b == addr_a:
+        addr_b = random.getrandbits(32)
+
+    await Timer(1, "ns")
+    await issue_command(dut, write_read=True,  addr=addr_a, data=data_a, latency=random.randint(1,2))
+    await RisingEdge(dut.clk)
+    await issue_command(dut, write_read=True,  addr=addr_b, data=data_b, latency=random.randint(1,2))
+    await issue_command(dut, write_read=False, addr=addr_a, data=data_a, latency=random.randint(1,2))
+    await RisingEdge(dut.clk)
+    await FallingEdge(dut.clk)
+    assert 0 == dut.valid
+    assert 0 == dut.stall
+
+# TODO: Implement different widths: 16b, 8b
 # @cocotb.test()
-async def test_write_instant_ack(dut) -> None:
+async def test_write_instant_ack_100(dut) -> None:
     cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
 
     await reset(dut)
@@ -167,7 +258,7 @@ async def test_write_instant_ack(dut) -> None:
         await Timer(1, "ps")
 
 # @cocotb.test()
-async def test_write_instant_ack(dut) -> None:
+async def test_write_instant_ack_200(dut) -> None:
     cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
 
     await reset(dut)
@@ -189,7 +280,7 @@ async def test_write_instant_ack(dut) -> None:
         await Timer(1, "ps")
 
 # @cocotb.test()
-async def test_write_instant_ack(dut) -> None:
+async def test_write_instant_ack_300(dut) -> None:
     cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
 
     await reset(dut)
@@ -245,13 +336,14 @@ def test_runner():
         project_path / f"{hdl_toplevel}.sv",
     ]
 
-    build_args = ["--trace", "--trace-structs"] if sim == "verilator" else []
+    # "-O3"
+    build_args = ["--trace", "--trace-structs", "--trace-params"] if sim == "verilator" else []
     runner = get_runner(sim)
     runner.build(
         verilog_sources=verilog_sources,
         vhdl_sources=[],
         hdl_toplevel=hdl_toplevel,
-        always=True,
+        # always=True,
         build_args=build_args,
         build_dir=f"build/{hdl_toplevel}",
     )
@@ -261,4 +353,6 @@ def test_runner():
 
 
 if __name__ == "__main__":
+    random.seed()
+
     test_runner()
